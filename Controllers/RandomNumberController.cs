c4307050ce42a6db6d4df82cebf61d710a840b82
rabbi mathake <mathakerabbi@hotmail.com>
2022-03-16 23:36:14 +0200

    get random number added

---
+++ Controllers/RandomNumberController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exercise2WebAPI.Models;
+
+namespace Exercise2WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomNumberController : Controller
+    {
+
+        [HttpGet]
+        // GET: RandomNumberController
+        public ActionResult<List<clsRandom>> Index()
+        {
+            Random random = new Random();
+            int num = random.Next(1,37);
+
+            clsRandom cls = new clsRandom();
+            cls.randomNum = num;
+            return new JsonResult(cls);
+        }
+
+    }
+}
---
+++ Models/clsRandom.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise2WebAPI.Models
+{
+    public class clsRandom
+    {
+        public int randomNum { get; set; }
+    }
+}
