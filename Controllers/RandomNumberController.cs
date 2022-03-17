772f34da07f9b27296e650d0cf0468450e7bf69b
rabbi mathake <mathakerabbi@hotmail.com>
2022-03-17 20:00:35 +0200

    Randomword method added

--- Controllers/RandomNumberController.cs
+++ Controllers/RandomNumberController.cs
@@ -5,24 +5,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Exercise2WebAPI.Models;
+using System.IO;
 
 namespace Exercise2WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class RandomNumberController : Controller
     {
 
-        [HttpGet]
+        [HttpGet("RandomNumber/GetRandomNumber")]
         // GET: RandomNumberController
-        public ActionResult<List<clsRandom>> Index()
+        public ActionResult<IEnumerable<clsRandom>> GetRandomNumber()
         {
             Random random = new Random();
             int num = random.Next(1,37);
 
-            clsRandom cls = new clsRandom();
-            cls.randomNum = num;
-            return new JsonResult(cls);
+            //return values
+            return new[]
+            {
+                 new clsRandom { randomNum = num }
+            };
+        }
+
+        [HttpGet("RandomNumber/GetRandomWord")]
+        // GET: RandomNumberController
+        public ActionResult<IEnumerable<clsRandomWord>> GetRandomWord()
+        {
+            string content = System.IO.File.ReadAllText("dws.txt");
+            Random random = new Random();
+            string[] split = content.Split(" ");
+            string randomString = split[random.Next(0, split.Length - 1)];
+
+            //return values
+            return new[]
+            {
+                 new clsRandomWord { word = randomString }
+            };
         }
 
     }
